4f8401bc006f380578652b7a2168623246ea90cd
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2025-02-19 08:54:22 -0700

    Update contacts.js

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,13 +1,5 @@
 import path from 'node:path';
 
-//getting the path of the `src` folder
-const pathToWorkDir = path.parse(process.cwd()).dir;
-const pathToDB = path.join(
-  pathToWorkDir,
-  'nodejs-hw-01',
-  'src',
-  'db',
-  'db.json',
-);
+const pathToDB = path.join(process.cwd(), 'src', 'db', 'db.json');
 
 export const PATH_DB = pathToDB;
